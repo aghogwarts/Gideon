678bf59f3c2dfba17b9c0e6afd845dc92d8bf809
Het Naik <76436467+awesomehet2124@users.noreply.github.com>
2021-07-21 11:47:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 import os
 
-from discord.ext.commands import bot
 
 client = commands.Bot(command_prefix=when_mentioned_or("."))
 token = os.getenv("Token")
@@ -17,12 +16,12 @@ async def whoami(ctx) :
     await ctx.send(f"You are {ctx.message.author.name}")
 
 @client.command()
-async def clear(ctx, amount=10) :
+async def clear(ctx, amount:int =10) 
     await ctx.channel.purge(limit=amount)
 
 @client.event
 async def on_message(message):
-    if client.user.mentioned_in(message):
+    if client.mention in message.contemt:
         await message.channel.send("Hey do !fix Premier League to see your fixes for the day")
         
     await bot.process_commands(message)    
