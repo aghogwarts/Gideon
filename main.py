ae122439c631b89b22eecac055439ebe663236a8
aghogwarts <87718788+aghogwarts@users.noreply.github.com>
2021-07-21 11:39:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,27 @@ import os
 
 from discord.ext.commands import bot
 
-client = commands.Bot(command_prefix=".")
+client = commands.Bot(command_prefix=when_mentioned_or("."))
 token = os.getenv("ODY2OTg4Mzk5MDMzMzE5NDQ1.YPaj3g.ZYdSEtney7HcOxA77fkUjD0rQqE")
 
 @client.event
 async def on_ready() :
     await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
-    print("I am online")
-
-@client.command()
-async def ping(ctx) :
-    await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
+    print("Ready to Deploy")
 
 @client.command(name="whoami")
 async def whoami(ctx) :
     await ctx.send(f"You are {ctx.message.author.name}")
 
 @client.command()
-async def clear(ctx, amount=3) :
+async def clear(ctx, amount=10) :
     await ctx.channel.purge(limit=amount)
 
+@client.event
+async def on_message(message):
+    if client.user.mentioned_in(message):
+        await message.channel.send("Hey do !fix Premier League to see your fixes for the day")
+        
+    await bot.process_commands(message)    
+    
 client.run("ODY2OTg4Mzk5MDMzMzE5NDQ1.YPaj3g.ZYdSEtney7HcOxA77fkUjD0rQqE")
